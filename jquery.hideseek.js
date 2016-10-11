a617e2a50ff93e6a0fc9f79808b9af6b4f672ce8
Cory Hake <coryhake@gmail.com>
2016-10-11 17:00:37 -0500

    Include support for a max_results property.
    To improve performance when dealing with large data sets, implement a maximum results flag. This will load and highlight only the first N results, with an option to click and load all remaining results.

--- jquery.hideseek.js
+++ jquery.hideseek.js
@@ -11,232 +11,298 @@
  *
  */
 
-  /* Sample html structure
+(function($, window, undefined) {
+    "use strict";
 
-  <input name="search" placeholder="Start typing here" type="text" data-list=".list">
-  <ul class="list">
-    <li>item 1</li>
-    <li>...</li>
-    <li><a href="#">item 2</a></li>
-  </ul>
+    $.fn.hideseek = function(options) {
 
-  or
+        var defaults = {
+            list: '.hideseek-data',
+            nodata: '',
+            attribute: 'text',
+            highlight: false,
+            ignore: '',
+            headers: '',
+            navigation: false,
+            ignore_accents: false,
+            hidden_mode: false,
+            min_chars: 1,
+            max_results: 10
+        };
 
-  <input name="search" placeholder="Start typing here" type="text" data-list=".list">
-  <div class="list">
-    <span>item 1</span>
-    <span>...</span>
-    <span>item 2</span>
-  </div>
+        var options = $.extend(defaults, options);
 
-  or any similar structure...
+        const addMaxResults = function addMax($this) {
+            $($this.opts.list).append('<div id="max-results" value="Max results limit" class="list-item"></div>');
+            const $maxRes = $('div#max-results');
+            $maxRes.hide()
+                .append('<span class="cat-span"><strong>Max results limit (' + $this.opts.max_results + ') reached</strong></span>')
+                .append('<span>Click to display all results for this search term. (This may take a while)</span>');
 
-  */
+            $this.closest('div').find('.description-list').find('#max-results').click(function() {
+                $this.trigger('updateListNoMax');
+            });
+        };
 
-;(function($, window, undefined) {
-  "use strict";
+        return this.each(function() {
 
-  $.fn.hideseek = function(options) {
+            var $this = $(this);
 
-    var defaults = {
-      list:           '.hideseek-data',
-      nodata:         '',
-      attribute:      'text',
-      highlight:      false,
-      ignore:         '',
-      headers:        '',
-      navigation:     false,
-      ignore_accents: false,
-      hidden_mode:    false,
-      min_chars:      1
-    };
+            $this.opts = [];
 
-    var options = $.extend(defaults, options);
+            $.map(['list', 'nodata', 'attribute', 'highlight', 'ignore', 'headers', 'navigation', 'ignore_accents', 'hidden_mode', 'min_chars', 'max_results'], function(val, i) {
+                $this.opts[val] = $this.data(val) || options[val];
+            });
 
-    return this.each(function() {
+            if ($this.opts.headers)
+                $this.opts.ignore += $this.opts.ignore ? ', ' + $this.opts.headers : $this.opts.headers;
 
-      var $this = $(this);
+            var $list = $($this.opts.list);
 
-      $this.opts = [];
+            if ($list.find('#max-results').length == 0) addMaxResults($this);
 
-      $.map( ['list', 'nodata', 'attribute', 'highlight', 'ignore', 'headers', 'navigation', 'ignore_accents', 'hidden_mode', 'min_chars'], function( val, i ) {
-        $this.opts[val] = $this.data(val) || options[val];
-      } );
+            if ($this.opts.navigation) $this.attr('autocomplete', 'off');
 
-      if ($this.opts.headers)
-        $this.opts.ignore += $this.opts.ignore ? ', ' + $this.opts.headers : $this.opts.headers;
+            if ($this.opts.hidden_mode) $list.children().hide();
 
-      var $list = $($this.opts.list);
+            $this.on('keyup', function(e) {
+                $this.trigger('updateList');
+            });
 
-      if ($this.opts.navigation)  $this.attr('autocomplete', 'off');
+            $this.on('updateList', function(e) {
+                updateList(e, true);
+            });
 
-      if ($this.opts.hidden_mode) $list.children().hide();
+            $this.on("updateListNoMax", function(e) {
+                updateList(e, false);
+            });
 
-      $this.keyup(function(e) {
+            const updateList = function listUpdate(event, max) {
+                if (e.keyCode != 38 && e.keyCode != 40 && e.keyCode != 13 && (e.keyCode != 8 ? $this.val().length >= $this.opts.min_chars : true)) {
 
-        if (e.keyCode != 38 && e.keyCode != 40 && e.keyCode != 13 && ( e.keyCode != 8 ? $this.val().length >= $this.opts.min_chars : true ) ) {
+                    var q = $this.val().toLowerCase();
+                    var matches = 0;
+                    var maxReached = false;
 
-          var q = $this.val().toLowerCase();
+                    if ( max ) {
+                        $list.children($this.opts.ignore.trim() ? ":not(" + $this.opts.ignore + ")" : '').removeClass('selected').each(function() {
 
-          $list.children($this.opts.ignore.trim() ? ":not(" + $this.opts.ignore + ")" : '').removeClass('selected').each(function() {
+                            $(this).hide();
 
-            var data = (
-                        $this.opts.attribute != 'text'
-                          ? ($(this).attr($this.opts.attribute) || '')
-                          : $(this).text()
+                        });
+                    }                    
+
+                    $list.children($this.opts.ignore.trim() ? ":not(" + $this.opts.ignore + ")" : '').removeClass('selected').each(function() {
+
+                        if (max) {
+
+                            if (($this.opts.max_results != 0) && (matches >= $this.opts.max_results)) {
+
+                                maxReached = true;
+
+                                return false;
+
+                            }
+
+                        }                       
+
+                        var data = (
+                            $this.opts.attribute != 'text' ? ($(this).attr($this.opts.attribute) || '') : $(this).text()
                         ).toLowerCase();
 
-            var treaty = data.removeAccents($this.opts.ignore_accents).indexOf(q) == -1 || q === ($this.opts.hidden_mode ? '' : false)
+                        var treaty = data.removeAccents($this.opts.ignore_accents).indexOf(q) == -1 || q === ($this.opts.hidden_mode ? '' : false);
 
-            if (treaty) {
+                        if (treaty) {
 
-              $(this).hide();
+                            $(this).hide();
 
-              $this.trigger('_after_each');
+                            $this.trigger('_after_each');
 
-            } else {
+                        } else {
 
-              $this.opts.highlight ? $(this).removeHighlight().highlight(q).show() : $(this).show();
+                            if ($this.opts.highlight) {
 
-              $this.trigger('_after_each');
+                                $(this).removeHighlight().highlight(q).show();
 
-            }
+                                $('#max-results').removeHighlight();
 
-          });
+                            } else {
 
-          // No results message
-          if ($this.opts.nodata) {
+                                $(this).show();
 
-            $list.find('.no-results').remove();
+                            }
 
-            if (!$list.children(':not([style*="display: none"])').length) {
+                            $this.trigger('_after_each');
 
-              $list
-                .children()
-                .first()
-                .clone()
-                .removeHighlight()
-                .addClass('no-results')
-                .show()
-                .prependTo($this.opts.list)
-                .text($this.opts.nodata);
+                            if (max) matches += 1;
 
-              $this.trigger('_after_nodata');
+                        }
 
-            }
+                    });
 
-          }
+                    if (max) {
 
-          // hide headers with no results
-          if ($this.opts.headers) {
-            $($this.opts.headers, $list).each(function() {
-              if (!$(this).nextUntil($this.opts.headers).not('[style*="display: none"],' + $this.opts.ignore).length) {
-                $(this).hide();
-              } else {
-                $(this).show();
-              }
-            });
-          }
+                        if (maxReached) {
 
-          $this.trigger('_after');
+                            $list.find('#max-results').show();
 
-        };
+                        } else {
 
-        // Navigation
-        function current(element) {
-          return element.children('.selected:visible');
-        };
+                            $list.find('#max-results').hide();
 
-        function prev(element) {
-          return current(element).prevAll(":visible:first");
-        };
+                        }
 
-        function next(element) {
-          return current(element).nextAll(":visible:first");
-        };
+                    } else {
 
-        if ($this.opts.navigation) {
+                        $list.find('#max-results').hide();
 
-          if (e.keyCode == 38) {
+                    }                    
 
-            if (current($list).length) {
+                    // No results message
+                    if ($this.opts.nodata) {
 
-              prev($list).addClass('selected');
+                        $list.find('.no-results').remove();
 
-              current($list).last().removeClass('selected');
+                        if (!$list.children(':not([style*="display: none"])').length) {
 
-            } else {
+                            $list
+                                .children()
+                                .first()
+                                .clone()
+                                .removeHighlight()
+                                .addClass('no-results')
+                                .show()
+                                .prependTo($this.opts.list)
+                                .text($this.opts.nodata);
 
-              $list.children(':visible').last().addClass('selected');
+                            $this.trigger('_after_nodata');
 
+                        }
+
+                    }
+
+                    // hide headers with no results
+                    if ($this.opts.headers) {
+                        $($this.opts.headers, $list).each(function() {
+                            if (!$(this).nextUntil($this.opts.headers).not('[style*="display: none"],' + $this.opts.ignore).length) {
+                                $(this).hide();
+                            } else {
+                                $(this).show();
+                            }
+                        });
+                    }
+
+                    $this.trigger('_after');
+
+                };
+
+                // Navigation
+                function current(element) {
+                    return element.children('.selected:visible');
+                };
+
+                function prev(element) {
+                    return current(element).prevAll(":visible:first");
+                };
+
+                function next(element) {
+                    return current(element).nextAll(":visible:first");
+                };
+
+                if ($this.opts.navigation) {
+
+                    if (e.keyCode == 38) {
+
+                        if (current($list).length) {
+
+                            prev($list).addClass('selected');
+
+                            current($list).last().removeClass('selected');
+
+                        } else {
+
+                            $list.children(':visible').last().addClass('selected');
+
+                        };
+
+                    } else if (e.keyCode == 40) {
+
+                        if (current($list).length) {
+
+                            next($list).addClass('selected');
+
+                            current($list).first().removeClass('selected');
+
+                        } else {
+
+                            $list.children(':visible').first().addClass('selected');
+
+                        };
+
+                    } else if (e.keyCode == 13) {
+
+                        if (current($list).find('a').length) {
+
+                            document.location = current($list).find('a').attr('href');
+
+                        } else {
+
+                            $this.val(current($list).text());
+
+                        };
+
+                    };
+
+                };
             };
 
-          } else if (e.keyCode == 40) {
+        });
 
-            if (current($list).length) {
+    };
 
-              next($list).addClass('selected');
-
-              current($list).first().removeClass('selected');
-
-            } else {
-
-              $list.children(':visible').first().addClass('selected');
-
-            };
-
-          } else if (e.keyCode == 13) {
-
-            if (current($list).find('a').length) {
-
-              document.location = current($list).find('a').attr('href');
-
-            } else {
-
-              $this.val(current($list).text());
-
-            };
-
-          };
-
-        };
-
-      });
-
-    });
-
-  };
-
-  $(document).ready(function () { $('[data-toggle="hideseek"]').hideseek(); });
-
+    $(document).ready(function() { $('[data-toggle="hideseek"]').hideseek(); });
 })(jQuery);
 
 /*
-
 highlight v4
-
 Highlights arbitrary terms.
-
 <http://johannburkard.de/blog/programming/javascript/highlight-javascript-text-higlighting-jquery-plugin.html>
-
 MIT license.
-
 Johann Burkard
 <http://johannburkard.de>
 <mailto:jb@eaio.com>
-
 */
-jQuery.fn.highlight=function(t){function e(t,i){var n=0;if(3==t.nodeType){var a=t.data.removeAccents(true).toUpperCase().indexOf(i);if(a>=0){var s=document.createElement("mark");s.className="highlight";var r=t.splitText(a);r.splitText(i.length);var o=r.cloneNode(!0);s.appendChild(o),r.parentNode.replaceChild(s,r),n=1}}else if(1==t.nodeType&&t.childNodes&&!/(script|style)/i.test(t.tagName))for(var h=0;h<t.childNodes.length;++h)h+=e(t.childNodes[h],i);return n}return this.length&&t&&t.length?this.each(function(){e(this,t.toUpperCase())}):this},jQuery.fn.removeHighlight=function(){return this.find("mark.highlight").each(function(){with(this.parentNode.firstChild.nodeName,this.parentNode)replaceChild(this.firstChild,this),normalize()}).end()};
+jQuery.fn.highlight = function(t) {
+    function e(t, i) {
+        var n = 0;
+        if (3 == t.nodeType) {
+            var a = t.data.removeAccents(true).toUpperCase().indexOf(i);
+            if (a >= 0) {
+                var s = document.createElement("mark");
+                s.className = "highlight";
+                var r = t.splitText(a);
+                r.splitText(i.length);
+                var o = r.cloneNode(!0);
+                s.appendChild(o), r.parentNode.replaceChild(s, r), n = 1
+            }
+        } else if (1 == t.nodeType && t.childNodes && !/(script|style)/i.test(t.tagName))
+            for (var h = 0; h < t.childNodes.length; ++h) h += e(t.childNodes[h], i);
+        return n
+    }
+    return this.length && t && t.length ? this.each(function() { e(this, t.toUpperCase()) }) : this
+}, jQuery.fn.removeHighlight = function() {
+    return this.find("mark.highlight").each(function() { with(this.parentNode.firstChild.nodeName, this.parentNode) replaceChild(this.firstChild, this), normalize() }).end()
+};
 
 // Ignore accents
 String.prototype.removeAccents = function(enabled) {
-  if (enabled) return this
-                      .replace(/[áàãâä]/gi,"a")
-                      .replace(/[éè¨ê]/gi,"e")
-                      .replace(/[íìïî]/gi,"i")
-                      .replace(/[óòöôõ]/gi,"o")
-                      .replace(/[úùüû]/gi, "u")
-                      .replace(/[ç]/gi, "c")
-                      .replace(/[ñ]/gi, "n");
-  return this;
-};
+    if (enabled) return this
+        .replace(/[áàãâä]/gi, "a")
+        .replace(/[éè¨ê]/gi, "e")
+        .replace(/[íìïî]/gi, "i")
+        .replace(/[óòöôõ]/gi, "o")
+        .replace(/[úùüû]/gi, "u")
+        .replace(/[ç]/gi, "c")
+        .replace(/[ñ]/gi, "n");
+    return this;
+}
