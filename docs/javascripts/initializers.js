ec3c6d27b14fc1324316ff5fcbc20da5bb0f5e9b
maxwroc <max.chodorowski@gmail.com>
2019-03-15 23:05:07 +0000

    Renaming demo folder to be able to use it as a homepage source

---
+++ docs/javascripts/initializers.js
@@ -0,0 +1,52 @@
+// Run javascript after DOM is initialized
+$(document).ready(function() {
+
+	$('#search').hideseek();
+
+	$('#search-highlight').hideseek({
+		highlight: true
+	});
+
+  $('#search-nodata').hideseek({
+    nodata: 'No results found'
+  });
+
+  $('#search-navigation').hideseek({
+    nodata: 'No results found',
+    navigation: true
+  });
+
+  $('#search-ignore').hideseek({
+    highlight: true,
+    ignore: '.ignore'
+  });
+
+  $('#search-ignore-accents').hideseek({
+    highlight: true,
+    ignore_accents: true
+  });
+
+  $('#search-headers').hideseek({
+    highlight: true,
+    headers: '.origin'
+  });
+
+  $('#search-hidden-mode').hideseek({
+    hidden_mode: true
+  });
+
+  $('#search-throttle').hideseek({
+    throttle: 2000
+  });
+
+  $('#search-custom-event').hideseek();
+  $('#search-custom-event').on("_after", function() {
+    alert('This alert comes after the search!')
+  });
+
+  $('#search-custom-event-2').hideseek();
+  $('#search-custom-event-2').on("_after_each", function() {
+    alert('This alert comes after the procession of each element!')
+  });
+
+});
