a05256b9fef3a81089d436bc06bdaefabf541d9e
maxwroc <max.chodorowski@gmail.com>
2019-03-29 00:35:01 +0000

    Polish accents

--- hideseek.helpers.js
+++ hideseek.helpers.js
@@ -8,19 +8,19 @@
    */
   function removeAccents(text) {
     return text
-      .replace(/[áàãâä]/gi, "a")
-      .replace(/[çč]/gi, "c")
-      .replace(/[éè¨ê]/gi, "e")
+      .replace(/[áàãâäą]/gi, "a")
+      .replace(/[çčć]/gi, "c")
+      .replace(/[éè¨êę]/gi, "e")
       .replace(/[íìïî]/gi, "i")
-      .replace(/[ĺľ]/gi, "l")
-      .replace(/[ñň]/gi, "n")
+      .replace(/[ĺľł]/gi, "l")
+      .replace(/[ñňń]/gi, "n")
       .replace(/[óòöôõ]/gi, "o")
       .replace(/[ŕř]/gi, "r")
-      .replace(/[š]/gi, "s")
+      .replace(/[šś]/gi, "s")
       .replace(/[ť]/gi, "t")
       .replace(/[úùüû]/gi, "u")
       .replace(/[ý]/gi, "y")
-      .replace(/[ž]/gi, "z");
+      .replace(/[žźż]/gi, "z");
   };
 
   hideseek.removeAccents = removeAccents;
